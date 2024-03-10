fb9d80c494dfbe8f96fa79dc6d63064d17d469fd
JinDoL1998 <jkhgfdds1@gmail.com>
2024-03-11 00:49:25 +0900

    게시판 테이블 수정시 상세조회 화면 수정되는 프로시저 위치 수정

--- SQL/data_output_query.sql
+++ SQL/data_output_query.sql
@@ -91,55 +91,6 @@ EXCEPTION
         DBMS_OUTPUT.PUT_LINE('An error occurred.');
 END;
 
--- 게시판 수정 프로시저 
-CREATE OR REPLACE PROCEDURE up_updTradeBoard(
-    ptrade_num IN NUMBER
-    ,pmember_num IN NUMBER
-    ,pselitem_ctgr_num IN NUMBER DEFAULT NULL
-    ,ptrade_title IN trade_board.trade_title%TYPE := NULL
-    ,ptrade_content trade_board.trade_content%TYPE := NULL
-    ,ptrade_price IN NUMBER DEFAULT NULL
-    ,ptrade_location trade_board.trade_location%TYPE := NULL
-    ,pupload_date IN DATE DEFAULT TO_DATE(SYSDATE, 'YY-MM-DD')
-)
-IS
-    vmember_num trade_board.member_num%TYPE;
-    MEMBER_NOT_MATCHED EXCEPTION;
-BEGIN
-    -- 해당 trade_num에 대한 member_num 값을 가져옵니다.
-    SELECT member_num INTO vmember_num
-    FROM trade_board
-    WHERE trade_num = ptrade_num;
-
-    -- 매개변수로 받은 member_num과 trade_board 테이블에서 가져온 member_num이 다른 경우 예외를 처리합니다.
-    IF vmember_num != pmember_num THEN
-        RAISE MEMBER_NOT_MATCHED;
-    END IF;
-
-    UPDATE trade_board
-    SET trade_title = NVL(ptrade_title, trade_title)
-        ,trade_content = NVL(ptrade_content, trade_content)
-        ,trade_price = NVL(ptrade_price, trade_price)
-        ,trade_location = NVL(ptrade_location, trade_location)
-        ,upload_date = NVL(pupload_date, upload_date)
-    WHERE trade_num = ptrade_num;
-    
-    COMMIT;
-    DBMS_OUTPUT.PUT_LINE('Trade details updated successfully.');
-EXCEPTION
-    WHEN NO_DATA_FOUND THEN
-        DBMS_OUTPUT.PUT_LINE('Trade number ' || ptrade_num || ' does not exist.');
-    WHEN MEMBER_NOT_MATCHED THEN
-        DBMS_OUTPUT.PUT_LINE('Member number ' || pmember_num || ' does not match.'); 
-    WHEN OTHERS THEN
-        DBMS_OUTPUT.PUT_LINE('An error occurred');
-END;
-
-BEGIN
-up_updtradeboard(1, pmember_num => 2
-,ptrade_title => '제목 수정2', pupload_date => '24/03/08');
-END;
-
 EXEC up_selTradeBoard(1);
 
 -- 동네생활 게시판 전체 조회
